2db619fb933c10536a21d562fadc2bf365d29c9f
Borsha <jannatulborsha17@gmail.com>
2018-04-04 19:55:25 -0700

    routine

---
+++ ToDoList.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+class ClassRoutine
+{
+    string sleepTime, wakeUpTime;
+    public:
+    void putSleepTime(string str)
+    {
+        sleepTime = str;
+    }
+
+    void putWakeUpTime(string str)
+    {
+        wakeUpTime  = str;
+    }
+
+    string getSleepTime()
+    {
+        return sleepTime;
+    }
+
+    string getWakeUpTime()
+    {
+        return wakeUpTime;
+    }
+}
+
+int main()
+{
+    ClassRoutine classRoutine;
+    classRoutine.putWakeUpTime("7.00 AM");
+    classRoutine.putSleepTime("12.00 AM");
+
+    cout << classRoutine.getWakeUpTime() << endl;
+    cout << classRoutine.getSleepTime() << endl;
+    return 0;
+}
+
